ddd0395b58a15ba1f9deaf450cb0cf367f12eeda
Наумов Никита Денисович <e.kug@mail.ru>
2023-09-11 12:58:39 +0300

    64

--- api.js
+++ api.js
@@ -1,31 +1,27 @@
-// Функция для инициализации клиента Google Sheets API
-function initClient() {
+function start() {
+    // Инициализируем Google Sheets API с вашими учетными данными
     gapi.client.init({
         apiKey: 'e99e5d7ec376b549ba587d7e23f98fc3ea6be205',
-        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
         clientId: '110295285327282200396',
-        scope: 'https://www.googleapis.com/auth/spreadsheets.readonly',
+        discoveryDocs: ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+        scope: "https://www.googleapis.com/auth/spreadsheets.readonly"
     }).then(function () {
-        // Вызываем функцию для загрузки данных после инициализации
-        getDataFromGoogleSheet();
-    });
-}
+        // Загружаем данные из таблицы
+        gapi.client.sheets.spreadsheets.values.get({
+            spreadsheetId: '1KeUxat0h4eddrYhc3UfY6QnzCiOUHHWTFd7886CGKvM/edit#gid=0',
+            range: 'Sheet1!C2' // Указываем диапазон (ячейку C2)
+        }).then(function(response) {
+            // Получаем значение из ячейки C2
+            var value = response.result.values[0][0];
 
-// Функция для загрузки данных из Google Таблицы
-function getDataFromGoogleSheet() {
-    gapi.client.sheets.spreadsheets.values.get({
-        spreadsheetId: '1KeUxat0h4eddrYhc3UfY6QnzCiOUHHWTFd7886CGKvM/edit#gid=0',
-        range: 'Sheet1!C2', // Здесь укажите диапазон ячейки, из которой нужно получить данные
-    }).then(function (response) {
-        var cellValue = response.result.values[0][0];
-        // Обновляем значение на вашей веб-странице
-        document.getElementById('num').innerHTML = cellValue;
-    }, function (error) {
-        console.error('Ошибка при загрузке данных: ', error.result.error.message);
-    });
-}
+            // Устанавливаем значение в переменную kvt1
+            var kvt1 = parseFloat(value); // Преобразовываем значение в число
 
-// Вызываем функцию для инициализации клиента Google Sheets API
-function handleClientLoad() {
-    gapi.load('client', initClient);
+            // Находим элемент с id "out"
+            var outElement = document.getElementById("out");
+
+            // Устанавливаем текст элемента равным значению kvt1
+            outElement.textContent = kvt1;
+        });
+    });
 }
